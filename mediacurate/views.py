d0f3e43e0b837d5e59ff3ba4a83ce93035e727c6
Josh Levinger <josh@levinger.net>
2011-10-25 19:03:43 -0700

    better time parsing

--- mediacurate/views.py
+++ mediacurate/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseRedirect,HttpResponseServerError
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.views.decorators.http import require_POST
 from django.core.urlresolvers import reverse
 from django.template.defaultfilters import slugify
-import datetime
+from datetime import datetime
+import dateutil.parser
 
 from django.contrib.comments.models import Comment
 from django.contrib.contenttypes.models import ContentType
@@ -62,11 +63,17 @@ def add(request):
             #create the objects with form.cleaned_data
             #can't use form.save() here, because it's not a modelform...
             print "form.cleaned_data",form.cleaned_data
-            #TODO: convert date_uploaded to consistent format
-            date_uploaded = form.cleaned_data['date_uploaded'] #TEMP, may fail
             
             location,new = Location.objects.get_or_create(name=form.cleaned_data['location'])
             embed = SavedEmbed.objects.get(url=form.cleaned_data['url'])
+            
+            #TODO: convert date_uploaded to consistent format
+            provider_name = embed.response['provider_name']
+            try:
+                date_uploaded = dateutil.parser.parse(form.cleaned_data['date_uploaded']) #TEMP, may fail
+            except ValueError,e:
+                return HttpResponseServerError("I don't know how to parse this date:",form.cleaned_data['date_uploaded'],"from",provider_name,e)
+                
             #could start with copy of form.cleaned_data, but that would pull in shared form fields
             media_dict = {
                 'date_uploaded':date_uploaded,
@@ -90,7 +97,7 @@ def add(request):
             comment_dict = {
                 'user_name':form.cleaned_data['name'],
                 'comment':form.cleaned_data['review'],
-                'submit_date':datetime.datetime.now(),
+                'submit_date':datetime.now(),
                 'ip_address':request.META['REMOTE_ADDR'],
                 'content_type':ContentType.objects.get(app_label="mediacurate",model="media"),
                 'object_pk':media.pk,
@@ -99,7 +106,7 @@ def add(request):
             print "comment_dict",comment_dict
             review = Comment(**comment_dict)
             review.save()
-            message = "Thanks for adding <a href='%s'>%s</a>. Want to add another video?" % (media.get_absolute_url(), media.title)
+            message = "Thanks for adding <a href='%s'>%s</a>. Want to add another?" % (media.get_absolute_url(), media.title)
             #give the user a new form
             form = AddForm()
     return render_to_response('add.html',
